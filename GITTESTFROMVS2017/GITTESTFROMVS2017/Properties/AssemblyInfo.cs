6f5864153b49a335782fc2c1d78d6cbfa21e2591
leelakrishna1982 <leelakrishna.g@outlook.com>
2018-06-14 23:23:54 -0700

    Commit from VS for GITTESTFROMVS2017

---
+++ GITTESTFROMVS2017/GITTESTFROMVS2017/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GITTESTFROMVS2017")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GITTESTFROMVS2017")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bc65d9ed-7ac2-404a-aa9d-6d9ada36324b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
